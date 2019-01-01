b8650e497637af3eac8b22134760f02c335406c1
prazd <alexokda@list.ru>
2019-01-01 17:39:30 +0300

    ...

--- examples/main.go
+++ examples/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	st "../stellar"
+	"encoding/json"
 	"github.com/krboktv/blockchain-swiss-knife/bitcoin"
 	"github.com/krboktv/blockchain-swiss-knife/dash"
 	"github.com/krboktv/blockchain-swiss-knife/ethereum"
@@ -16,8 +17,9 @@ import (
 
 func main() {
 
-	eth()
-	fmt.Print("\n")
+	//eth()
+	//fmt.Print("\n")
+
 	btc()
 	fmt.Print("\n")
 	//dash_()
@@ -66,6 +68,14 @@ func btc() {
 	address, _ := bitcoin.GetAddress(privateKey)
 
 	balanceTest := bitcoin.GetBalance("18bXSCSXiTD3DB3XEz851VpB4ZK49rkprT")
+
+	transaction, err := bitcoin.CreateTransaction("5HusYj2b2x4nroApgfvaSfKYZhRbKFH41bVyPooymbC6KfgSXdD", "1KKKK6N21XKo48zWKuQKXdvSsCf95ibHFa", 91234, "81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	data, _ := json.Marshal(transaction)
+
 	fmt.Println("---Bitcoin---")
 	fmt.Print("Private Key: ")
 	fmt.Println(hex.EncodeToString(privateKey))
@@ -76,6 +86,8 @@ func btc() {
 	fmt.Println("Account for balance test: 18bXSCSXiTD3DB3XEz851VpB4ZK49rkprT")
 	fmt.Print("Test Balance: ")
 	fmt.Println(balanceTest)
+	fmt.Print("Create Raw tx:")
+	fmt.Println(string(data))
 	fmt.Println("---Bitcoin---")
 }
 
